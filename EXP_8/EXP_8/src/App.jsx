4db759430091e31bc0eb045f0234d39fa8ccbbed
Ridah <ridah.25b01010047@abes.ac.in>
2026-05-15 23:42:38 +0530

    Experiment - 8

--- EXP_8/EXP_8/src/App.jsx
+++ EXP_8/EXP_8/src/App.jsx
@@ -1,30 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
-import { counter } from './components/Counter.jsx'
+import Counter from './components/Counter.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="container">
-            <div className="title">React Counter Application</div>
-
-            <h1 className="display">{count}</h1>
-
-            <div className="buttons">
-                <button className="inc" onClick={increment}>INCREMENT +</button>
-                <button className="dec" onClick={decrement}>DECREMENT -</button>
-                <button className="reset" onClick={reset}>RESET</button>
-            </div>
-
-
-        </div>
+      <Counter />
     </>
   )
 }
 
-export default App
+export default App;
--- EXP_8/EXP_8/src/components/Counter.jsx
+++ EXP_8/EXP_8/src/components/Counter.jsx
@@ -1,24 +1,29 @@
 import { useState } from "react";
 
 function Counter() {
-    const[count, setCount] = useState(0)
+    const [count, setCount] = useState(0);      //setting a default state
 
-    const increment = () => setCount(count + 1)
-    const decrement = () => setCount(count - 1)
-    const reset = () => setCount(0)
+    function increment() {
+        setCount(count + 1);
+    }
+
+    function decrement() {
+        setCount(count - 1);
+    }
+
+    function reset() {
+        setCount(0);
+    }
 
     return (
-        <div className="container">
-            <div className="title">React Counter Application</div>
+        <div>
+            <h2>React counter application</h2>
 
-            <h1 className="display">{count}</h1>
+            <h2>{count}</h2>
 
-            <div className="buttons">
-                <button className="inc" onClick={increment}>INCREMENT +</button>
-                <button className="dec" onClick={decrement}>DECREMENT -</button>
-                <button className="reset" onClick={reset}>RESET</button>
-            </div>
-
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+            <button onClick={reset}>reset</button>
 
         </div>
     )
